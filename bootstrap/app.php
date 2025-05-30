09ed0b944bc88814dcdbd548e8f12601ed5cc60a
nckmackenzie <nicholas.mackenzie1@gmail.com>
2025-05-30 16:49:56 +0300

    feat: register middleware to convert case

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\ConvertCase;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Application;
@@ -16,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->statefulApi();
+        $middleware->append(ConvertCase::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (QueryException $e){
